9c47c066359f7e9d4840263792b45c7d010917e6
Mario Iglesias <marioip@gmail.com>
2015-09-13 18:42:50 +0100

    add modules browserify, jade, livereload, sass, uglify - starting workflow basic jade, sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,38 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	jade = require('gulp-jade'),
+	livereload = require('gulp-livereload')
+	sass = require('gulp-sass');
 
-gulp.task('default', []);
+var env = process.env.NODE_ENV || 'development';
+var outputDir = 'dist/';
+
+gulp.task('jade', function(){
+	return gulp.src('src/**/*.jade')
+	.pipe(jade())
+	.pipe(gulp.dest(outputDir))
+	.pipe(livereload());
+});
+
+gulp.task('sass', function(){
+	var config = {};
+
+	if (env === 'development'){
+		config.sourceComments = 'map';
+	}
+
+	if (env === 'production'){
+		config.outputStyle = 'compressed';
+	}
+
+	return gulp.src('src/sass/main.scss')
+		.pipe(sass(config))
+		.pipe(gulp.dest(outputDir+'/css'))
+		.pipe(livereload());
+});
+
+gulp.task('watch', function(){
+	livereload.listen();
+	gulp.watch('src/**/*.jade', ['jade']);
+});
+
+gulp.task('default', ['jade','sass','watch']);
